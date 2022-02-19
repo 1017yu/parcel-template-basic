250a9db45c58654a8919a39d248fbd6e7bac186b
Kled <1017yu@daum.net>
2022-02-19 11:06:47 +0900

    Initial commit

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+// rc(Runtime Configuration)
+
+// ESM (ECMA Script Module)(in Browser)
+// CommonJS (in NodeJS)
+
+// import autoprefixer from 'autoprefixer' ===
+const autoprefixer = require('autoprefixer');
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# parcel-template-basic
+ 
